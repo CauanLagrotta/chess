3329963da60b3c21cc0eb4e7f37195e2a0ffb24a
CauanLagrotta <caun06lagrotta@gmail.com>
2024-05-23 17:54:40 -0300

    commitando

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
-import Header from './components/header '
-import './index.css'
-function App() {
-    <Header/>
+import React from 'react';
+import Header from './components/header.jsx';
+import './index.css';
 
+function App() {
+    return (
+        <div>
+            <Header />
+            
+            <div className="">
+                <h1 className="flex text-3xl font-bold justify-center mt-5" >O Que é Xadrez?</h1>
+                
+            </div>
+            
+
+        </div>
+    );
 }
 
-export default App
+export default App;
